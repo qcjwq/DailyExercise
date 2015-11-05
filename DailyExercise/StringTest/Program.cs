11e5ba10c518df4ae0e7b815fedfcd27cfdb72bd
chen_jw <chen_jw@Ctrip.com>
2015-11-05 10:06:59 +0800

    string test

---
+++ DailyExercise/StringTest/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = "";
+            string b = string.Empty;
+            string c = null;
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+
+            var temp = a.Equals(b);
+            temp = a.Equals(c);
+
+            temp = b.Equals(a);
+            temp = b.Equals(c);
+
+            temp = c.Equals(a);//“System.NullReferenceException”类型的未经处理的异常在 StringTest.exe 中发生 其他信息: 未将对象引用设置到对象的实例。
+            temp = c.Equals(b);//“System.NullReferenceException”类型的未经处理的异常在 StringTest.exe 中发生 其他信息: 未将对象引用设置到对象的实例。
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ DailyExercise/StringTest/StringTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace StringTest
+{
+    public class StringTest
+    {
+        public void Test()
+        {
+            string a = "";
+            string b = string.Empty;
+            string c = null;
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+        }
+    }
+}
