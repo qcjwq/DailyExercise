bb998d15d2b37b6a4debdf476e262ac591c639f1
chen_jw <chen_jw@Ctrip.com>
2016-04-21 10:01:37 +0800

    Modify String

---
+++ DailyExercise/ModifyString/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ModifyString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string csharp = "I love CSharp";
+            Console.Write(csharp + " = ");
+
+            unsafe
+            {
+                fixed (char* pJava = csharp)
+                {
+                    pJava[7] = 'J';
+                    pJava[8] = 'a';
+                    pJava[9] = 'v';
+                    pJava[10] = 'a';
+                    pJava[11] = ' ';
+                    pJava[12] = ' ';
+                }
+            }
+
+            Console.WriteLine(csharp);
+            Console.WriteLine("I love CSharp".Equals("I love Java  "));
+            Console.ReadKey();
+        }
+    }
+}
